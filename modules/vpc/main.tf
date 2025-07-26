d01ed12e50b266c25364502f75cfcd390ae4bf2c
Jithu T Narayanan <105491982+jithunarayanan@users.noreply.github.com>
2025-07-26 21:27:14 +0530

    Initial commit

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,7 +7,7 @@ locals {
   common_tags  = merge(var.default_tags, {})
 }
 
-resource "aws_vpc" "this" {
+resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -16,14 +16,14 @@ resource "aws_vpc" "this" {
 })
 }
 
-resource "aws_internet_gateway" "this" {
-  vpc_id = aws_vpc.this.id
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
   tags = local.common_tags
 }
 
 resource "aws_subnet" "public" {
   count                   = length(var.public_azs)
-  vpc_id                  = aws_vpc.this.id
+  vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.public_subnet_cidrs[count.index]
   availability_zone       = var.public_azs[count.index]
   map_public_ip_on_launch = true
@@ -34,7 +34,7 @@ resource "aws_subnet" "public" {
 
 resource "aws_subnet" "private" {
   count             = length(var.private_azs)
-  vpc_id            = aws_vpc.this.id
+  vpc_id            = aws_vpc.vpc.id
   cidr_block        = var.private_subnet_cidrs[count.index]
   availability_zone = var.private_azs[count.index]
 
@@ -44,27 +44,28 @@ resource "aws_subnet" "private" {
 }
 
 resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.this.id
+  vpc_id = aws_vpc.vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.this.id
+    gateway_id = aws_internet_gateway.igw.id
   }
   tags = merge(local.common_tags, {
-  Name = "${local.tag_prefix}-nacl"
+  Name = "${local.tag_prefix}-public-route-table-${count.index + 1}"
   })
+  
 }
 
 resource "aws_route_table" "private" {
   count  = length(var.private_azs)
-  vpc_id = aws_vpc.this.id
+  vpc_id = aws_vpc.vpc.id
 
 #   route {
 #     cidr_block = "0.0.0.0/0"
 #     gateway_id = aws_nat_gateway.this[count.index].id
 #   }
   tags = merge(local.common_tags, {
-  Name = "${local.tag_prefix}-nacl"
+  Name = "${local.tag_prefix}-private-route-table-${count.index + 1}"
   })
 }
 
@@ -80,15 +81,17 @@ resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.private[count.index].id
 }
 
-resource "aws_network_acl" "this" {
-  vpc_id = aws_vpc.this.id
+resource "aws_network_acl" "nacl" {
+  vpc_id = aws_vpc.nacl.id
 
-  tags = local.common_tags
+  tags = merge(local.common_tags, {
+  Name = "${local.tag_prefix}-nacl-${count.index + 1}"
+  })
 }
 
 resource "aws_network_acl_rule" "ingress" {
   count          = length(var.nacl_ingress_rules)
-  network_acl_id = aws_network_acl.this.id
+  network_acl_id = aws_network_acl.nacl.id
   egress         = false
 
   rule_number    = var.nacl_ingress_rules[count.index].rule_number
@@ -101,7 +104,7 @@ resource "aws_network_acl_rule" "ingress" {
 
 resource "aws_network_acl_rule" "egress" {
   count          = length(var.nacl_egress_rules)
-  network_acl_id = aws_network_acl.this.id
+  network_acl_id = aws_network_acl.nacl.id
   egress         = true
 
   rule_number    = var.nacl_egress_rules[count.index].rule_number
@@ -115,11 +118,11 @@ resource "aws_network_acl_rule" "egress" {
 resource "aws_network_acl_association" "public" {
   count           = length(var.public_azs)
   subnet_id       = aws_subnet.public[count.index].id
-  network_acl_id  = aws_network_acl.this.id
+  network_acl_id  = aws_network_acl.nacl.id
 }
 
 resource "aws_network_acl_association" "private" {
   count           = length(var.private_azs)
   subnet_id       = aws_subnet.private[count.index].id
-  network_acl_id  = aws_network_acl.this.id
+  network_acl_id  = aws_network_acl.nacl.id
 }
