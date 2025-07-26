1ff139a9d7180bd3ec81e09ae57083fe5891eaea
Jithu T Narayanan <105491982+jithunarayanan@users.noreply.github.com>
2025-07-26 20:08:50 +0530

    Initial commit

---
+++ modules/vpc/output.tf
@@ -0,0 +1,29 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.this.id
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = [for subnet in aws_subnet.public : subnet.id]
+}
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = [for subnet in aws_subnet.private : subnet.id]
+}
+
+output "public_route_table_id" {
+  description = "Route table ID for the public subnets"
+  value       = aws_route_table.public.id
+}
+
+output "private_route_table_ids" {
+  description = "List of route table IDs for private subnets"
+  value       = [for rt in aws_route_table.private : rt.id]
+}
+
+output "network_acl_id" {
+  description = "Network ACL ID used by the subnets"
+  value       = aws_network_acl.this.id
+}
